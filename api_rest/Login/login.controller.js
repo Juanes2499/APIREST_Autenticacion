3ccec7d5fbb0409a67b85cb98b3f71cfbc7782f4
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-03-09 22:26:44 -0500

    Se crea los endpoint para enviar un contraseña temporal y para actualizar la contraseña

--- api_rest/Login/login.controller.js
+++ api_rest/Login/login.controller.js
@@ -1,6 +1,7 @@
 const {
     autenticar_ByEmail,
-    solicitar_cambio_contrasena
+    solicitar_cambio_contrasena,
+    actualizar_contrasena
 } = require('./login.service');
 const { hashSync, genSaltSync, compareSync } = require("bcrypt");
 const { sign } = require("jsonwebtoken");
@@ -11,13 +12,14 @@ module.exports ={
 
         const body = req.body;
 
-        autenticar_ByEmail(body, (err, results, state) => {
+        autenticar_ByEmail(body, (err, internalCode, results, state) => {
 
             if (err) {
                 return res.status(401).json({
                     success: state,
                     statusCode: 401,
                     message: err,
+                    code: internalCode,
                     return: err,
                 });
             }
@@ -27,15 +29,18 @@ module.exports ={
                     success: state,
                     statusCode: 401,
                     message: err,
+                    code: internalCode,
                     return: err,
                 });
             }
 
-            const result = compareSync(body.password, results.PASSWORD);
+            const result = compareSync(body.password, results.PASSWORD_AUTENTICACION);
 
             if (result) {
 
                 results.PASSWORD = undefined;
+                result.PASSWORD_AUTENTICACION = undefined;
+                result.PASSWORD_ACTIVA = undefined;
                 
                 const payloald = results;
 
@@ -63,6 +68,7 @@ module.exports ={
                     success: state,
                     statusCode:200,
                     message: "login successfully",
+                    code:internalCode,
                     email: results.EMAIL,
                     token: jsontoken,
                     expedicion_token: fechaHoraExpedicion,
@@ -72,6 +78,7 @@ module.exports ={
                 return res.status(401).json({
                     success: state,
                     statusCode: 401,
+                    code: 'LOGIN_ERROR_04',
                     message: "Invalid email or password",
                 });
             }
@@ -118,6 +125,52 @@ module.exports ={
                 success: state,
                 statusCode:200,
                 message: `The email password updadte for EMAIL:  ${body.email} has been completed`
+            });
+        });
+    },
+    actualizarContrasena: (req, res) => {
+        
+        const body = req.body;
+
+        const parametrosEndpoint = {  
+            email: true,
+            old_password: true,
+            password_auth: true  
+        };
+        
+        const arrayParametrosJsonComparar = Object.keys(body);
+        
+        const verificarParametro = MensajeverificarParametrosJson(parametrosEndpoint, arrayParametrosJsonComparar)
+
+        if(verificarParametro.error === true || verificarParametro.messageFaltantes != null || verificarParametro.messageMalEscritos != null ){
+            
+            const errorData = {
+                mensaje_retornado: `${verificarParametro.messageFaltantes}, please set a all required parameters`
+            }
+
+            return res.status(500).json({
+                success: false,
+                statusCode: 500,
+                message: errorData.mensaje_retornado
+            })
+        }
+
+        actualizar_contrasena(body, (err, result, state) => {
+
+            if(state === false){
+                console.log(err);
+                return res.status(403).json({
+                    success: state, 
+                    statusCode: 403,
+                    message: "Database login error - error in actualizarContrasena",
+                    return: err
+                });
+            }
+
+            return res.status(200).json({
+                success: state,
+                statusCode:200,
+                message: `The password updadte for EMAIL:  ${body.email} has been finished`
             });
         });
     },
--- api_rest/Login/login.router.js
+++ api_rest/Login/login.router.js
@@ -1,10 +1,12 @@
 const {
     login,
-    solicitarCambioContrasena
+    solicitarCambioContrasena,
+    actualizarContrasena,
 } = require('./login.controller');
 const router = require('express').Router();
 
 router.post("/", login);
 router.post("/emailPassword", solicitarCambioContrasena);
+router.put("/updatePassword", actualizarContrasena);
     
 module.exports = router;
