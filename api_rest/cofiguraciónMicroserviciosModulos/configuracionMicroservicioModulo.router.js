e08ecb4a08263ca8112d8108c488ce97fffd178e
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-03-27 19:53:06 -0500

    modificación adicionales configuración microservicios modulos

--- api_rest/cofiguraciónMicroserviciosModulos/configuracionMicroservicioModulo.router.js
+++ api_rest/cofiguraciónMicroserviciosModulos/configuracionMicroservicioModulo.router.js
@@ -4,11 +4,23 @@ const {
     eliminarConfiguracioMicroservicioModuloByID
 } = require('./configuracionMicroservicioModulo.controller');
 
+const {
+    consultarMicroservicioDinamico
+} = require('../Microservicios/microservicio.controller');
+
+const {
+    consultarModuloDinamico
+} = require('../Modulos/modulo.controller');
 
 const router = require('express').Router();
 
 router.post('/', crearConfiguracioMicroservicioModulo);
 router.post('/get', consultarConfiguracioMicroservicioModuloDinamico);
 router.post('/delete', eliminarConfiguracioMicroservicioModuloByID);
+
+//Adicioanales
+router.post('/microservicios/get', consultarMicroservicioDinamico);
+router.post('/modulos/get', consultarModuloDinamico);
+
 
 module.exports = router;
