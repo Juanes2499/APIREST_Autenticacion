0a3421c281af01266f80a209e9c6984a2bd02db9
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-03-27 20:00:58 -0500

    Modificación adicionales configuración usuarios

--- api_rest/configuracionUsuarios/configuracionUsuarios.router.js
+++ api_rest/configuracionUsuarios/configuracionUsuarios.router.js
@@ -3,10 +3,23 @@ const {
     consultarConfiguracionUsuarioDinamico,
     eliminarConfiguracionUsuarioByID
 } = require('./configuracionUsuarios.controller');
+
+const {
+    consultarMicroservicioDinamico
+} = require('../Microservicios/microservicio.controller');
+
+const {
+    consultarModuloDinamico
+} = require('../Modulos/modulo.controller');
+
 const router = require('express').Router();
 
 router.post('/', crearConfiguracionUsuarios);
 router.post('/get', consultarConfiguracionUsuarioDinamico);
-router.post('/delete', eliminarConfiguracionUsuarioByID )
+router.post('/delete', eliminarConfiguracionUsuarioByID);
+
+//Adicioanales
+router.post('/microservicios/get', consultarMicroservicioDinamico);
+router.post('/modulos/get', consultarModuloDinamico);
 
 module.exports = router;
