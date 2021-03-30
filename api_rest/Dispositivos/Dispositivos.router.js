c89feb2b4d9d59d129575eeca8cecedc0566507a
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-03-29 22:10:46 -0500

    Actualización adicionales dispositivos router

--- api_rest/Dispositivos/Dispositivos.router.js
+++ api_rest/Dispositivos/Dispositivos.router.js
@@ -4,11 +4,19 @@ const {
     actualizarDispositivoById,
     eliminarDispositivoById
 } = require('./Dispositivos.controller');
+
+const {
+    consultarMicroservicioDinamico
+} = require('../Microservicios/microservicio.controller');
+
 const router = require('express').Router();
 
 router.post("/", crearDispositivo);
 router.post("/get", consultarDispositivos);
 router.put("/", actualizarDispositivoById),
 router.post("/delete", eliminarDispositivoById);
+
+//Adicioanales
+router.post('/microservicios/get', consultarMicroservicioDinamico);
     
 module.exports = router;
