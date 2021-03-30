46335613d063065ffb58da7379482a268e33dcad
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-03-29 22:59:05 -0500

    Actualización service dispositivos

--- api_rest/Dispositivos/Dispositivos.service.js
+++ api_rest/Dispositivos/Dispositivos.service.js
@@ -19,19 +19,19 @@ module.exports={
         pool.query(
             queryConsultarExistenciaUsuario,
             [data.email_responsable],
-            (error, resultUser) => {
+            (error, resultDevice) => {
 
                 if (error){
                     return callback(`There is/are error(s), please contact with the administrator`, null, false);
                 }
 
-                if(resultUser.length === 0){
+                if(resultDevice.length === 0){
 
                     return callback(`The user with email: ${data.email_responsable} was not found`, null, false);
 
-                }else if (resultUser.length > 0){
+                }else if (resultDevice.length > 0){
 
-                    const resultUserToJson = JSON.parse(JSON.stringify(resultUser))[0];
+                    const resultDeviceToJson = JSON.parse(JSON.stringify(resultDevice))[0];
 
                     const queryVerificarExistenciaMicroservicio = `
                         SELECT 
@@ -124,7 +124,7 @@ module.exports={
                                             sendEmail(
                                                 data.email,
                                                 'Creación dispositivo ResCity',
-                                                `Apreciado usuario ResCity: ${resultUserToJson.NOMBRES} ${resultUserToJson.APELLIDOS}, se ha registrado un dispositivo bajo su 
+                                                `Apreciado usuario ResCity: ${resultDeviceToJson.NOMBRES} ${resultDeviceToJson.APELLIDOS}, se ha registrado un dispositivo bajo su 
                                                 responsabilidad, por favor cambiar la contraseña del dispositivo usando esta contraseña inicial: ${passWordAletoriaEncrypted}. 
                                                 El token del dispositivo es: ${jsonTokenDispositivo}. Recuerde que la contraseña y el token pueden ser cambiados cuando el 
                                                 usuario lo desee. el ID del dispositivo, el token y la contraseña son necesarias para para el envío de datos.`,
@@ -308,6 +308,166 @@ module.exports={
                             return callback(null, null, true);
                         }
                     );
+                }
+            }
+        )
+    },
+    solicitar_cambio_contrasena: (data, callback) => { 
+
+        const queryConsutarExistenciaDispositivo = `
+            SELECT 
+                * 
+            FROM DISPOSITIVOS 
+            WHERE 
+                ID_DISPOSITIVO = ? AND EMAIL_RESPONSABLE = ?
+        `;
+
+        pool.query(
+            queryConsutarExistenciaDispositivo,
+            [data.id_dispositivo, data.email_responsable],
+            (error, resultDevice) => {
+
+                if (error){
+                    return callback(`There is/are error(s), please contact with the administrator`, null, false);
+                }
+                
+                if(resultDevice.length === 0){
+
+                    return callback(`The device with ID_DISPOSITIVO: ${data.id_dispositivo} does not exist `, null, false);
+
+                }else if(resultDevice.length > 0){
+
+                    const device = JSON.parse(JSON.stringify(resultDevice))[0];
+
+                    let passWordAletoriaEncrypted = '';
+                    let passWordAletoria = base64url(crypto.randomBytes(15));
+                    const salt = genSaltSync(10);
+                    const encriptPass = new Promise((resolve, reject)=>{
+                        passWordAletoriaEncrypted = hashSync(passWordAletoria,salt)
+                        resolve()
+                    })
+
+                    encriptPass
+                        .then()
+                        .catch((err)=>{
+                            console.log(err);
+                        });
+
+                    queryActualizarDispositivoCambioContrasena = `
+                        UPDATE DISPOSITIVOS
+                            SET PASSWORD = ?,
+                                PASSWORD_ACTIVA = false,
+                                PASSWORD_AUTENTICACION = null,
+                                FECHA_ACTUALIZACION_PASSWORD = CURDATE(),
+                                HORA_ACTUALIZACION_PASSWORD = CURTIME()
+                            WHERE ID_DISPOSITIVO = ? AND EMAIL_RESPONSABLE = ?`;
+
+                    pool.query(
+                        queryActualizarDispositivoCambioContrasena,
+                        [passWordAletoriaEncrypted,data.id_dispositivo,data.email_responsable],
+                        (error, result) => {
+
+                            if (error) {
+                                return callback(`The device with ID_DISPOSITIVO: ${data.id_dispositivo} can not update the password`, null, false);
+                            }else{
+                                sendEmail(
+                                    data.email,
+                                    'Recuperación contraseña dispositivo ResCity',
+                                    `Apreciado usuario ResCity, Ha solicitado recuperar la contraseña del dispositivo. Por favor ingresar a la plataforma ResCity Core, al servicio Autenticación, al módulo Dispositivos y con la siguiente contraseña temporal: ${passWordAletoria} realice la actuliazación del dispositivo. Durante este periodo su el dispositivo permanece inactivo hasta que realice el cambio de contraseña. Si presenta algun tipo de inconveniente comunicarse con el administrador de la plataforma. Muchas gracias.`,
+                                    (result) => {
+                                        if(result === false) {
+                                            return callback('Email could not be sended', null, false)
+                                        }else{
+                                            return callback(null, null, true)
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    )
+                }
+            }
+        )
+    },
+    actualizar_contrasena: (data, callback) => { 
+
+        const queryConsutarExistenciaDispositivo = `
+            SELECT 
+                * 
+            FROM DISPOSITIVOS 
+            WHERE 
+                ID_DISPOSITIVO = ? AND EMAIL_RESPONSABLE = ?
+        `;
+
+        pool.query(
+            queryConsutarExistenciaDispositivo,
+            [data.id_dispositivo, data.id_dispositivo, data.email_responsable],
+            (error, resultDevice) => {
+
+                if (error){
+                    return callback(`There is/are error(s), please contact with the administrator`, null, false);
+                }
+                
+                if(resultDevice.length === 0){
+
+                    return callback(`The device with ID_DISPOSITIVO: ${data.id_dispositivo} does not exist `, null, false);
+
+                }else if(resultDevice.length > 0){
+
+                    const device = JSON.parse(JSON.stringify(resultDevice))[0];
+
+                    const result = compareSync(data.old_password, device.PASSWORD);
+
+                    if(result){
+
+                        const salt = genSaltSync(10);
+                        const encriptPass = new Promise((resolve, reject)=>{
+                            data.password_auth = hashSync(data.password_auth,salt)
+                            resolve()
+                        })
+
+                        encriptPass
+                            .then()
+                            .catch((err)=>{
+                                console.log(err);
+                            });
+
+
+                        const queryActualizarDispositivoCambioContrasena = `
+                            UPDATE DISPOSITIVOS
+                                SET 
+                                    PASSWORD_ACTIVA = true,
+                                    PASSWORD_AUTENTICACION = ?,
+                                    FECHA_ACTUALIZACION_PASSWORD = CURDATE(),
+                                    HORA_ACTUALIZACION_PASSWORD = CURTIME()
+                                WHERE ID_DISPOSITIVO = ? AND EMAIL_RESPONSABLE = ?`;
+
+                        pool.query(
+                            queryActualizarDispositivoCambioContrasena,
+                            [data.password_auth, data.id_dispositivo, data.email_responsable],
+                            (error, result) => {
+                                
+                                if (error) {
+                                    return callback(`The device with ID_DISPOSITIVO: ${data.id_dispositivo} can not update the password`, null, false);
+                                }else{
+                                    sendEmail(
+                                        data.email,
+                                        'Cambio contraseña dispositivo ResCity',
+                                        `Apreciado usuario ResCity, la contraseña del dispositivo with ID: ${data.id_dispositivo} ha sido actualizada correctamente. A partir de este momento puede publicar datos si el dispositivo esta activado por el administrador de la plataforma.`,
+                                        (result) => {
+                                            if(result === false) {
+                                                return callback('Email could not be sended', null, false)
+                                            }else{
+                                                return callback(null, null, true)
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }else {
+                        return callback(`The the old password does not match with the temporal password saved for the ID_DISPOSITIVO: ${data.id_dispositivo}`, null, true)
+                    }
                 }
             }
         )
