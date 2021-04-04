36962328598220ac51969df35d23141a498e3018
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-04-04 16:56:20 -0500

    CI/CD 16

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,8 +2,8 @@
 #Stopping existing node servers
 echo "Deteniendo servicios desplegados en NodeJS"
 
-if [ sudo lsof -t -i:3020 ]; then
-  sudo kill -9 $(sudo lsof -t -i:3000) 
-else
-  echo "Not working"
-fi
+# if [ sudo lsof -t -i:3020 ]; then
+#   sudo kill -9 $(sudo lsof -t -i:3000) 
+# else
+#   echo "Not working"
+# fi
